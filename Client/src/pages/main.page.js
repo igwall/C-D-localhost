f2439d448987a52deb9a808147482df2779ec5fe
Maroane <maroane.hamida@etu.umontpellier.fr>
2017-12-15 14:41:09 +0100

    enhance UI Icons

--- Client/src/pages/main.page.js
+++ Client/src/pages/main.page.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import PageLayout from '../layouts/page'
+import Icon from '../components/UI/Icon/Icon'
 
 export default (props) => (
   <PageLayout>
+    <Icon color="red"/>
     <h1>Hurray</h1>
   </PageLayout>
 )
