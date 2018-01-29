62e0e06711fd4601dc6dbf773925a9ba4638f006
yassinenaitssi <naitssi.y@gmail.com>
2018-01-29 18:07:06 +0100

    Back&FrontDone

--- Client/src/components/Library/composent/OutSide.js
+++ Client/src/components/Library/composent/OutSide.js
@@ -1,34 +1,31 @@
 import React from 'react'
 import styles from './composent.styles'
-import {connect} from 'react-redux'
-@connect(store => {
-  return {
-    Videos: store.artistFetched.artist
-  }
-})
 
 export default class News extends React.Component {
   render () {
-    const books = [ 1, 2, 3, 4, 5, 6, 7 ]
+    let books = []
+    books = this.props.references
     return (<div className='host'>
-      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-      <div className= 'videos'>
+      <link rel='stylesheet' type='text/css' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css' />
+      <link rel='stylesheet' type='text/css' href='https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css' />
+      <div className='videos'>
         <div>
-          <div className='videos-title'><h2>Ailleurs nous aimons,</h2></div>
+          <div className='videos-title'><h2>Ailleurs nous aimons...</h2></div>
           <table>
             <thead>
               <tr>
-                <th> Nom </th>
+                <th> Titre </th>
                 <th> Description </th>
+                <th> Lien </th>
               </tr>
             </thead>
             <tbody>
               {
                 books.map((book, i) =>
                   <tr key={i}>
-                    <td>Bill Gates</td>
-                    <td>55577854</td>
+                    <td>{book.title}</td>
+                    <td>{book.description}</td>
+                    <td>{book.link}</td>
                   </tr>
                 )
               }
