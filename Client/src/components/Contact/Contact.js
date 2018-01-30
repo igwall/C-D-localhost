536230aea03d64abd4182225168cebdd12343189
yassinenaitssi <naitssi.y@gmail.com>
2018-01-30 10:24:03 +0100

    ContactUs

---
+++ Client/src/components/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Contact.styles'
+
+export default class Contact extends React.Component {
+  render () {
+    return (<div className='host'>
+      <div className='header'>
+        <div className='PageTitle'><h1>Nous contacter</h1></div>
+      </div>
+      <style jsx>{styles}</style>
+    </div>
+
+    )
+  }
+}
