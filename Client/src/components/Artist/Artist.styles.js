fa7d61e26b6eee7794683d40bc06dc93dd5a372f
yassinenaitssi <naitssi.y@gmail.com>
2018-01-30 11:59:35 +0100

    ContactUsFront

--- Client/src/components/Artist/Artist.styles.js
+++ Client/src/components/Artist/Artist.styles.js
@@ -12,5 +12,211 @@ export default css`
 
 }
 
+.head {
+  background-color: ${constants.SECONDARY_COLOR};
+  width : 100%;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  padding-top: 3%;
+  padding-right: 5%;
+  min-height: 100px;
+  text-align: justify;
+  transition: min-height 0.75s; 
+}
 
+.headLeftSide{
+  diplay: inline-block;
+  width : 35%;
+}
+.headRightSide{
+  diplay: inline-block;
+  width : 65%;
+}
+
+.artistPicture {
+  width: 40%;
+  margin-left: 18%;
+  height: 270px;
+
+}
+.suite {
+  display:none;
+}
+
+.LireSuite {
+  font-style: italic;
+  font-size: 12px;
+}
+.artistDiplome{
+  margin-top: 5%;
+  font-size: 10px;
+  font-style: italic;
+  text-align: justify;
+  color: #a1a1a7;
+}
+.artistName {
+  text-align: center;
+  margin-right: 25%;
+  margin-top: 5%;
+  font-size: 20px;
+  font-weight: bold;
+}
+.artistBio{
+  display: inline-block;
+  width: 100%;
+  height:100px;
+  text-align: justify;
+}
+.middle{ 
+  padding-top: 5%;
+}
+.middleHead{
+  display: flex;
+  padding : 4%;
+}
+.books{
+  padding-bottom: 5%;
+  margin-left: 10%;
+  width: 50%;
+  display: inline-block;
+}
+.books-title{
+  margin-bottom: 13%;
+  font-size: 20px;
+  font-weight: bolder;
+}
+.book{
+  max-height: 180px;
+  overflow-y: auto;
+  margin-left: 5%;
+}
+.iteem{
+  margin-bottom: 5%;
+}
+.diplomes{
+  padding-bottom: 5%;
+  margin-left: 25%;
+  width: 25%;
+  display : inline-block;
+}
+.diplome-title{
+margin-bottom: 5%;
+font-size: 20px;
+font-weight: bolder;
+}
+.diplome{
+max-height: 140px;
+overflow-y: auto;
+margin-left: 5%;
+}
+.pictures-title{
+  padding-bottom: 5%;
+  margin-left: 4%;
+  font-size: 20px;
+  font-weight: bolder;
+}
+.pictures{
+  width: 80%
+  margin-left: 10%;
+
+}
+
+.pic{
+  width:100%;
+  height:100%
+}
+.picture{
+  width: 80%;
+  margin-left: 64%;
+}
+.pictureLegend{
+  text-align: justify;
+  font-size: 12px;
+  padding-top: 3%;
+  padding-left: 40%;
+}
+.videos-title{
+  padding-top: 5%
+  padding-bottom: 5%;
+  margin-left: 4%;
+  font-size: 20px;
+  font-weight: bolder;
+}
+.videos{
+  width: 80%
+  margin-left: 10%;
+  padding-top: 2%;
+}
+.references-table-title{
+  margin-left: 15%;
+  padding-bottom: 2%;
+  Font-Weight: Bold;
+  font-size: 20px;
+}
+.references-list{
+  display: inline-block;
+  width: 100%;
+  padding-left: 5%;
+}
+
+
+.reference {
+  padding: 10px;
+  display: flex;
+  width:85%;
+  margin-left: 10%;
+  align-items: center;
+  overflow-y: auto;
+  height: 213px;
+  
+}
+.tableBody {
+  padding: 10px;
+  display: flex;
+  width:85%;
+  margin-left: 10%;
+  align-items: center;
+  overflow-y: auto;
+  height: 213px;
+  
+}
+table{
+    width: 100%;
+}
+table, th, td {
+  border: 1px solid white;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 5px;
+  text-align: left;    
+}
+.audios{
+  padding-top: 5%;
+  margin-left: 13%;
+  width: 50%;
+  display: inline-block;
+}
+.audio-title{
+  margin-bottom: 5%;
+  font-size: 20px;
+  font-weight: bolder;
+}
+.audio{
+  max-height: 180px;
+  overflow-y: auto;
+  margin-left: 5%;
+}
+.iteeem{
+  display : flex;
+  margin-bottom: 5%;
+}
+.lecteur{
+  display: inline-block;
+  padding-left: 200px;
+}
+.audio-label{
+  display: inline-block;
+}
 `
--- Client/src/components/Contact/Contact.js
+++ Client/src/components/Contact/Contact.js
@@ -4,8 +4,54 @@ import styles from './Contact.styles'
 export default class Contact extends React.Component {
   render () {
     return (<div className='host'>
-      <div className='header'>
+      <div className='sideBarre'>
         <div className='PageTitle'><h1>Nous contacter</h1></div>
+        <div className='Description'> Un concept qui se développe petit à petit sur différents sites : les champs forment un texte à trous cohérent, que l’utilisateur doit remplir. Un bon moyen de faire participer activement l’utilisateur, sans devoir remplir un formulaire rébarbatif. </div>
+      </div>
+      <div className='Main'>
+        <div className='Forms'>
+          <div className='Forms-Item'>
+            <div className='item'>
+              <input
+                type='text'
+                placeholder='Email'
+                // onChange={this.checkName.bind(this)}
+                ref={n => { this.name = n }}
+              />
+            </div>
+            <div className='item'>
+              <input
+                type='text'
+                placeholder='Nom et Prénom'
+                // onChange={this.checkName.bind(this)}
+                ref={n => { this.name = n }}
+              />
+            </div>
+            <div className='item'>
+              <input
+                type='text'
+                placeholder='Numéro de telephone'
+                // onChange={this.checkName.bind(this)}
+                ref={n => { this.name = n }}
+              />
+            </div>
+            <input
+              type='text'
+              placeholder='Sujet'
+              // onChange={this.checkName.bind(this)}
+              ref={n => { this.name = n }}
+            />
+            <div className='Message'>
+              <input
+                type='text'
+                placeholder='Message'
+                // onChange={this.checkName.bind(this)}
+                ref={n => { this.name = n }}
+              />
+            </div>
+          </div>
+
+        </div>
       </div>
       <style jsx>{styles}</style>
     </div>
